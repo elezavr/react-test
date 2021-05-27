0f708eb9e673a62dd98d9edd6166ac3f2db697ab
elezavr <77399006+elezavr@users.noreply.github.com>
2021-05-27 18:36:14 +0700

    changes test

--- src/App.js
+++ src/App.js
@@ -12,10 +12,8 @@ const App = () => {
 
         web3.eth.getBalance(address).then(
             result => {
-
                 const balance = web3.utils.fromWei(result, "ether");
                 console.log(balance);
-                
             },
             error => {
                 console.log(error);
