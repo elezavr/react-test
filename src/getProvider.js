2bfa7513de6936873efbaccd112ba126966c34ad
elezavr <77399006+elezavr@users.noreply.github.com>
2021-05-28 00:25:45 +0700

    update

--- src/getProvider.js
+++ src/getProvider.js
@@ -3,17 +3,9 @@ import Web3 from 'web3';
 const apiKey = 'infura_api_key';
 
 export const getProvider = () => {
-    const options = {
-        headers: [
-            {
-                name: 'Access-Control-Allow-Origin',
-                value: '*'
-            }
-        ]
-    }
 
     const provider = new Web3.providers.HttpProvider(
-        `https://mainnet.infura.io/v3/${apiKey}`, options
+        `https://mainnet.infura.io/v3/${apiKey}`
     );
 
     return provider;
